39ed51c21ad27bffe8e1aaf27300dd4341ab70ce
alexandrapanas <alexandra.panas@why.de>
2023-11-02 19:04:00 +0100

    refactor: update script

--- ga-tracking.js
+++ ga-tracking.js
@@ -1,7 +1,10 @@
-window.dataLayer = window.dataLayer || []
-function gtag() {
-  dataLayer.push(arguments)
-}
-gtag('js', new Date())
-
-gtag('config', 'G-N4G30G4Y36')
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "G-N4G30G4Y36");
